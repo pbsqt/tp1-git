5fd35f83fffdffaefc2a28f8a5a76395867701fb
Pierre Bosquet <pierre.bosquet@esme.fr>
2023-10-03 15:46:51 +0200

    indextest3

--- test2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Message Updated");
-    return 0;
-}
--- test3.c
+++ test3.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    printf("Message Updated");
+    printf("Gamberge");
     return 0;
 }
